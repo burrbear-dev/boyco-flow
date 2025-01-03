ae5aa50de25bec78ca60a33a48fe05b6a85f9b40
shu <shu@burr>
2025-01-03 17:50:26 +0700

    🥝 new deployment

--- script/BoycoBurrZap.s.sol
+++ script/BoycoBurrZap.s.sol
@@ -5,9 +5,11 @@ pragma abicoder v2;
 import "forge-std/Script.sol";
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
+import {IERC20} from "@balancer-labs/v2-interfaces/contracts/solidity-utils/openzeppelin/IERC20.sol";
+
 import {BoycoBurrZap} from "../src/BoycoBurrZap.sol";
 
-contract BoycoBurrZapScript is Script {
+contract BoycoBurrZapScript is Script, Test {
     // cArtio environment
     address constant VAULT = 0x398D4CFB5D29d18BaA149497656904F2e8814EFb;
     address constant NECT_USDC_HONEY_POOL = 0xFbb99BAD8eca0736A9ab2a7f566dEbC9acb607f0;
@@ -21,18 +23,21 @@ contract BoycoBurrZapScript is Script {
     function run() public {
         vm.startBroadcast();
         // cartio deployment
+        // allow new TWAP observations every 10 minutes
+        // set window to 24 hours
         BoycoBurrZap zap = new BoycoBurrZap(
-            USDC, NECT_USDC_HONEY_POOL, BALANCER_QUERIES, HONEY_FACTORY, NECT, PSM_BOND_PROXY, 24 hours, 24
+            USDC, NECT_USDC_HONEY_POOL, BALANCER_QUERIES, HONEY_FACTORY, NECT, PSM_BOND_PROXY, 24 hours, 24 * 6
         );
         // whitelist sender
         zap.whitelist(msg.sender);
         // setup TWAP
         uint256 bpt = zap.queryDeposit(1e6, msg.sender);
+        // we need to broadcast 2 transactions with observations where to
+        // enable `consult` to work
+        // BUT (currently, at least) Foundry script includes both calls
+        // in the same transaction, even if we use vm.broadcast(), so we
+        // need to do the 2nd call manually
         zap.recordObservation(bpt);
-        zap.recordObservation(bpt);
-
         vm.stopBroadcast();
     }
-
-    function _deployCartio() internal {}
 }
